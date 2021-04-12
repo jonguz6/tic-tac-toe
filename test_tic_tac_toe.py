ed3df952261b9150bccaf452cb439ed5bbc862cd
Jonatan <jonguz6@wp.pl>
2021-04-12 19:59:49 +0200

    test(TTT): test function for creating a board

---
+++ test_tic_tac_toe.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+from tic_tac_toe import create_board
+
+class TestTicTacToe(TestCase):
+    def setUp(self) -> None:
+        self.board = [' | | ', '-+-+-', ' | | ', '-+-+-', ' | | ']
+
+
+    def test_create_board(self):
+        self.assertEqual(create_board(), self.board)
---
+++ tic_tac_toe.py
@@ -0,0 +1,6 @@
+def create_board():
+    return None
+
+
+if __name__ == '__main__':
+    pass
